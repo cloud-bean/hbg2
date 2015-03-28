cc5dcc085200ff356bc8ef591134021c6811ef32
hygkui <fevernova.life@gmail.com>
2015-03-28 21:44:13 +0800

    set the dependenices of ngQuickDate

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'hbg';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router' ];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ngQuickDate'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
