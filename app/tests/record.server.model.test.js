383112e512cd66990fd18f5bd4f4db25fcc79610
hygkui <fevernova.life@gmail.com>
2015-07-07 20:03:58 +0800

    ok with model test for record

--- app/tests/record.server.model.test.js
+++ app/tests/record.server.model.test.js
@@ -4,61 +4,89 @@
  * Module dependencies.
  */
 var should = require('should'),
-	mongoose = require('mongoose'),
-	User = mongoose.model('User'),
-	Record = mongoose.model('Record');
+    mongoose = require('mongoose'),
+    Record = mongoose.model('Record'),
+    Member = mongoose.model('Member'),
+    Inventory = mongoose.model('Inventory');
 
 /**
  * Globals
  */
-var user, record;
+
+var record, 
+    member, 
+    inventory;
 
 /**
  * Unit tests
  */
 describe('Record Model Unit Tests:', function() {
-	beforeEach(function(done) {
-		user = new User({
-			firstName: 'Full',
-			lastName: 'Name',
-			displayName: 'Full Name',
-			email: 'test@test.com',
-			username: 'username',
-			password: 'password'
-		});
+    beforeEach(function(done) {
 
-		user.save(function() { 
-			record = new Record({
-				name: 'Record Name',
-				user: user
-			});
+        inventory = new Inventory({
+            inv_code: '4100012345678',
+            name: 'test for inventory- books title'
+        });
 
-			done();
-		});
-	});
+        member = new Member({
+            phone_number: '14220987643',
+            baby_name: 'baby-test-name',
+            card_number: '987654321'
+        });
 
-	describe('Method Save', function() {
-		it('should be able to save without problems', function(done) {
-			return record.save(function(err) {
-				should.not.exist(err);
-				done();
-			});
-		});
+        var tasks_count = 2;
+        var tasks_completed = 0;
 
-		it('should be able to show an error when try to save without name', function(done) { 
-			record.name = '';
+        var isCompleted = function() {
+            tasks_completed++;
+            if (tasks_completed === tasks_count) {
+                record = new Record({
+                    member: member,
+                    inventory: inventory
+                });
+                done();
+            }
+        };
 
-			return record.save(function(err) {
-				should.exist(err);
-				done();
-			});
-		});
-	});
+        inventory.save(isCompleted);
+        member.save(isCompleted);
 
-	afterEach(function(done) { 
-		Record.remove().exec();
-		User.remove().exec();
+    });
 
-		done();
-	});
+    describe('Method Save', function() {
+
+        it('should be able to save without problems', function(done) {
+
+            return record.save(function(err) {
+                if (err) console.log(err);
+                should.not.exist(err);
+                done();
+            });
+        });
+
+        it('should not be able to save record without member', function(done) {
+            record.member = null;
+            return record.save(function(err) {
+                should.exist(err);
+                done();
+            });
+        });
+
+        it('should not be able to save record without inventory', function(done) {
+            record.inventory = null;
+            return record.save(function(err) {
+                should.exist(err);
+                done();
+            });
+        });
+
+
+    });
+
+    afterEach(function(done) {
+        Record.remove().exec();
+        Member.remove().exec();
+        Inventory.remove().exec();
+        done();
+    });
 });
