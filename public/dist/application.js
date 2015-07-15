47b44a57dc8e03615744d1df6737456e92f4e7ec
xinnix2000 <xinnix2000@gmail.com>
2015-07-15 14:09:40 +0800

    rebuild

--- public/dist/application.js
+++ public/dist/application.js
@@ -877,6 +877,10 @@ angular.module('records').config(['$stateProvider',
 			url: '/records/create',
 			templateUrl: 'modules/records/views/create-record.client.view.html'
 		})
+		.state('listRecord', {
+			url: '/records/list',
+			templateUrl: 'modules/records/views/list-record.client.view.html'
+		})
 		.state('returnBook', {
 					url: '/records/return',
 					templateUrl: 'modules/records/views/return-record.client.view.html'
@@ -948,6 +952,7 @@ angular.module('records').controller('RecordsController', ['$scope', '$timeout',
         // Find a list of Records
         $scope.find = function() {
             $scope.records = Records.query();
+            console.log( $scope.records );
         };
 
         // Find existing Record
