50cd7e156da05f32193739a86572af3c188ea911
hygkui <fevernova.life@gmail.com>
2015-12-23 22:21:50 +0800

    reset path

--- bin/db_backup.sh
+++ bin/db_backup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo '####backup db start >>>>>>>'
 dbpath=$(date +"%y%m%d%H%M%S")
-mongodump ${1} -o ../DBBackup/${dbpath}
-zip -rj ../DBBackup/${2}_db_${dbpath}.zip ../DBBackup/${dbpath}
-rm -rf ../DBBackup/${dbpath}
+mongodump ${1} -o ./DBBackup/${dbpath}
+zip -rj ./DBBackup/${2}_db_${dbpath}.zip ./DBBackup/${dbpath}
+rm -rf ./DBBackup/${dbpath}
 echo '####backup db finished <<<<'
 echo ''
 
